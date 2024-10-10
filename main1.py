4e80a61c4cff3bdfe272134ce96b96197d95d74c
Gawana89 <akuka89@mail.ru>
2024-10-10 21:45:40 +0300

    add dict and func

--- main1.py
+++ main1.py
@@ -1,22 +1,11 @@
-f = open('figure.txt', 'w')
+issue = {}
 
-def stroka(wide):
-    stroka_f = ""
-    for i in range(wide):  
-        if i == 0 or i == wide-1:
-            stroka_f += '@'* wide
-            stroka_f += '\n'
-        else:
-            stroka_f += '@'+ ' '*(wide-2) +'@'
-            stroka_f += '\n'
-       
-    return stroka_f
+issue['Завтрак'] = ['8:30',False, False]
+issue['Уборка'] = ['10:30',False, False]
+issue['Корм.Кота'] = ['12:00',False, False]
 
+for key,value in issue.items():
+    print(key,value)
 
-for i in stroka(5):
-    f.write(i)
-f.close()
-f = open('figure.txt', 'r')
-st = f.read()
-for i in st:
-    print(i,end='')
+delete = issue.pop('Завтрак')
+print(delete)
